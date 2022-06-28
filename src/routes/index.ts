52e3841ac05010b9b7f4d8ebdc7e023100bd481c
mr-ema <antonio.a.v.c2017@gmail.com>
2022-06-27 20:54:24 -0400

    Update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,8 +9,20 @@ router.get('/', function(req: Request, res: Response, next: NextFunction) {
 
 const auth = require('../middleware/auth');
 
-router.get('/status', auth, (req: Request, res: Response) => {
-  res.render('status', { status: 'Authenticated' });
+interface CustomRequest extends Request {
+  user?: {
+    user_id: string,
+    email: string
+  }
+}
+
+router.get('/status', auth, (req: CustomRequest, res: Response) => {
+  if(req.user)  {
+    const user = req.user;
+    res.render('status', { status: 'Authenticated', user: user });
+
+  }else res.status(500).send('something went wrong');
+  
 });
 
 module.exports = router;
